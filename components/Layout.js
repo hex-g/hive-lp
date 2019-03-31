0a1030400150ef77fc1e84c31f55692a74ab9e35
Matheus Bolognini <matheusbolognini79@gmail.com>
2019-03-31 12:18:02 -0300

    Update Layout properties

--- components/Layout.js
+++ components/Layout.js
@@ -1,27 +1,27 @@
 import React, { Fragment } from 'react'
 import styled, { createGlobalStyle, ThemeProvider } from 'styled-components'
+import reset from 'styled-reset'
 import Header from './Header'
 
 export const theme = {
   rajah: '#F6BD60',
   burntSienna: '#E9724C',
-  athensGray: '#E8E9EB',
+  wildSand: '#F5F4F4',
   hippieBlue: '#5C9EAD',
   outerSpace: '#313638',
-  primaryFont: 'sans-serif',
-  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+  primaryFont: 'Open Sans',
+  cardBoxShadow: '6px 8px 20px 0 rgba(0, 0, 0, 0.08)',
+  buttonBoxShadow: '0 2px 10px 2px rgba(0, 0, 0, 0.2)',
+  inputBoxShadow: '0 2px 10px 2px rgba(0, 0, 0, 0.07)'
 }
 
 export const GlobalStyle = createGlobalStyle`
+  ${reset}
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
   body {
-    margin: 0;
-    font-family: ${props => props.theme.primaryFont};
+    font-family: ${props => props.theme.primaryFont}, sans-serif;
     color: ${props => props.theme.outerSpace};
-    background-color: ${props => props.theme.lightgrey};
-  }
-
-  ul {
-    margin: 0;
+    background-color: ${props => props.theme.wildSand};
   }
 `
 
