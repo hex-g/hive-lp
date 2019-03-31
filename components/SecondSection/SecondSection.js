2b2dfb3a1897f01e2db837fd0fc0373e8fae9180
CaioMassayuki <japa.zito@hotmail.com>
2019-03-31 14:08:42 -0300

    Standardized fro content in Zeplin and remove Media Query

--- components/SecondSection/SecondSection.js
+++ components/SecondSection/SecondSection.js
@@ -1,30 +1,30 @@
 import React from 'react'
-import { Section, Title, SquareContainer, Square, SquareTwo, Image, TitleSquare, Text } from './SecondSection.style'
+import { Section, Title, SquareContainer, Square, ImageOne, ImageTwo, ImageThree, TitleSquare, Text } from './SecondSection.style'
 
 const SecondSection = () => {
-    return (
-        <Section>
-            <Title>O que é Hive ?</Title>
-            <SquareContainer>
-                <Square>
-                    <Image />
-                    <TitleSquare>Organize suas anotações</TitleSquare>
-                    <Text>Um local dedicado a suas anotações.</Text>
-                </Square>
-                <Square>
-                    <Image />
-                    <TitleSquare>Salve seus arquivos</TitleSquare>
-                    <Text>Armazene suas anotações na nuvem.</Text>
-                </Square>
-                <Square>
-                    <Image />
-                    <TitleSquare>Suporte a Markdown</TitleSquare>
-                    <Text>Anotações que suportam Markdown.</Text>
-                </Square>
-            </SquareContainer>
-            <Text>Bacon ipsum dolor amet tri-tip turkey pork loin boudin tenderloin strip steak tail fatback jerky pig.Bacon ipsum dolor amet tri-tip turkey pork loin boudin tenderloin strip steak tail fatback jerky pig.</Text>
-        </Section>
-    )
+  return (
+    <Section>
+      <Title>O que é Hive ?</Title>
+      <SquareContainer>
+        <Square>
+          <ImageOne />
+          <TitleSquare>Personalizável</TitleSquare>
+          <Text>A Hive é facilmente personalizável de forma nativa para atender o contexto da instituição.</Text>
+        </Square>
+        <Square>
+          <ImageTwo />
+          <TitleSquare>Open Source</TitleSquare>
+          <Text>A plataforma tem o código aberto e sugestões são sempre bem-vindas!</Text>
+        </Square>
+        <Square>
+          <ImageThree />
+          <TitleSquare>Gamificação</TitleSquare>
+          <Text>Na Hive os alunos constroem seus perfis de acordo com o desempenho acadêmico.</Text>
+        </Square>
+      </SquareContainer>
+      <Text>Hive é uma plataforma de <b>gestão acadêmica</b> para alunos e professores. Na ferramenta, o corpo docente disponibiliza e gerencia conteúdos para suas turmas, enquanto os estudantes compartilham suas anotações e interagem no fórum de perguntas. A Hive acredita que <b>aprender é uma constante</b>, por isso disponibilizamos ferramentas de produtividade e elementos de gamificação para facilitar a sinergia dentro da instituição de ensino.</Text>
+    </Section>
+  )
 }
 
 export default SecondSection
--- components/SecondSection/SecondSection.style.js
+++ components/SecondSection/SecondSection.style.js
@@ -7,35 +7,21 @@ export const Section = styled.section`
   flex-direction: column;
   align-items: center;
   background-color: #F5F5F5;
-
-  @media (min-width: 992px){
-
-  }
 `
 
 export const Title = styled.h1` 
-  font-size: 100px;
+  font-size: 40px;
   font-weight: 900;
-
-  @media (min-width: 992px){
-   font-size: 40px;
-   font-weight: 900;
-  }
 `
 
 export const SquareContainer = styled.div`
-  width: 90%;
+  width: 80%;
   height: 100%;
   display: flex;
-  flex-direction: column;
+  flex-direction: row;
   align-items: center;
-
-  @media (min-width: 992px){
-    flex-direction: row;  
-    color: white;
-    justify-content: space-around;
-    margin-bottom: 100px;
-  }
+  justify-content: space-around;
+  margin-bottom: 85px;
 `
 
 export const Square = styled.div`
@@ -44,46 +30,43 @@ export const Square = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-
-  @media (min-width: 992px){
-    background-color: #212121;
-    border-radius: 15px;
-    box-shadow: 3px 3px 10px rgba(51, 51, 51, 50%);
-    margin: 5px;
-    padding: 15px;
-  }
+  margin: 5px;
+  padding: 15px;
 `
 
-export const Image = styled.img`
-  width: 500px;
-  height: 500px;
-  background-image: url('https://i.kym-cdn.com/photos/images/newsfeed/000/887/044/67a.jpg');
-  border-radius: 50%;
+export const ImageOne = styled.div`
+  width: 150px;
+  height: 150px;
+  background-image: url('https://cdn.zeplin.io/5c9fc47c50d7a876f773d81f/assets/7764FC57-21E8-4D09-8FF8-B1AE5D9F1691.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+`
 
-  @media (min-width: 992px){
-    width: 200px;
-    height: 200px;
-  }
+export const ImageTwo = styled.div`
+  width: 150px;
+  height: 150px;
+  background-image: url('https://cdn.zeplin.io/5c9fc47c50d7a876f773d81f/assets/912E7210-2034-4DD3-95DE-8270E2FAEF27.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
+`
+
+export const ImageThree = styled.div`
+  width: 150px;
+  height: 150px;
+  background-image: url('https://cdn.zeplin.io/5c9fc47c50d7a876f773d81f/assets/9CFB96E9-4A63-4D1D-B5B3-C76799B8A14D.svg');
+  background-repeat: no-repeat;
+  background-size: cover;
 `
 
 export const TitleSquare = styled.h2`
-  font-size: 55px;
+  font-size: 30px;
   font-weight: 900;
   margin-bottom: 50px;
-
-  @media (min-width: 992px){
-    font-size: 30px;
-  }
 `
 
 export const Text = styled.p`
-  font-size: 45px;
   text-align: center;
-  margin: 0 0 150px 0;
-  
-  @media (min-width: 992px){
-    width: 50%;
-    font-size: 20px;
-    margin: 0 0 50px 0;
-  }
+  width: 70%;
+  font-size: 20px;
+  margin: 0 0 50px 0;
 `
