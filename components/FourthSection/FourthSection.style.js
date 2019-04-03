97de972a07e71d1f8d1704de4896e83b4952f90f
giuliamaia2 <giuliamaia2@gmail.com>
2019-04-02 21:26:57 -0300

    Add fourth section

---
+++ components/FourthSection/FourthSection.style.js
@@ -0,0 +1,54 @@
+import styled from 'styled-components'
+
+export const Section = styled.section`
+width: 100vw;
+height: 50%;
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: #F5F5F5;
+`
+
+
+export const Description = styled.p`
+  text-align: center;
+  width: 450px;
+  font-size: 20px;
+  margin: 50px 0 0 0;
+`
+
+export const Container = styled.div`
+  text-align: center;
+  width: 700px;
+  font-size: 20px;
+  margin: 50px 0 50px 0;
+`
+
+export const TextInput = styled.input`
+  text-align: left;
+  width: 450px;
+  font-size: 20px;
+  margin: 20px;
+  height: 30px;
+  padding: 10px;
+  border-radius: 7px;
+  outline: none;
+  border: none;
+  box-shadow: ${props => props.theme.buttonBoxShadow};
+`
+
+export const Button = styled.button`
+  width: 140px;
+  height: 50px;
+  border-radius: 7px;
+  box-shadow: ${props => props.theme.buttonBoxShadow};
+  text-align: center;
+  font-size: 20px;
+  margin: 20px;
+  background: ${props => props.theme.hippieBlue};
+  color: ${props => props.theme.wildSand};
+  font-weight: bold;
+  outline: none;
+  border: none;
+  cursor: pointer;
+`
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Layout from '../components/Layout'
 import FirstSection from '../components/FirstSection'
 import SecondSection from '../components/SecondSection'
+import FourthSection from '../components/FourthSection';
 
 const Index = () => {
   return (
     <Layout>
       <FirstSection />
       <SecondSection />
+      <FourthSection />
     </Layout>
   )
 }
