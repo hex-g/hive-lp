60c76bf3814bf478078f34106fcc7028c71950f7
henrique-gs <henriquegodez@gmail.com>
2019-04-09 21:25:34 -0300

    Add the hexagon SVG in the first section

--- components/FirstSection/FirstSection.js
+++ components/FirstSection/FirstSection.js
@@ -1,10 +1,18 @@
 import React from 'react'
-import { Section } from './FirstSection.style'
+import { Section, TextoPrincipal, TextoPrincipal__Bold , Hexagono } from './FirstSection.style'
+import SvgMapping from '../SvgMapping'
 
 const FirstSection = () => {
   return (
     <Section>
-
+      <Hexagono>
+          <SvgMapping name="hexagon" />
+      </Hexagono>
+      <TextoPrincipal>
+          Maior
+          <TextoPrincipal__Bold> produtividade acadêmica </TextoPrincipal__Bold> 
+          para o aluno e para o professor. 
+        </TextoPrincipal>
     </Section>
   )
 }
--- components/FirstSection/FirstSection.style.js
+++ components/FirstSection/FirstSection.style.js
@@ -4,3 +4,20 @@ export const Section = styled.section`
   height: calc(100vh - 81px);
   background-color: ${props => props.theme.athensGray};
 `
+
+export const TextoPrincipal = styled.div `
+  width: 447px;
+  font-family: ${props => props.theme.secondaryFont};
+  font-size: 24px;
+  color: #ffffff;
+`
+
+export const TextoPrincipal__Bold = styled.span`
+  font-weight: bold;
+`
+
+export const Hexagono = styled.div`
+  position: absolute;
+  left: -300px;
+  z-index: -1;
+`
