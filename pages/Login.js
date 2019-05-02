b17c86246e9d389db211f05c6ecff674a24d2283
Caio Massayuki <32188335+CaioMassayuki@users.noreply.github.com>
2019-05-02 14:19:59 -0300

    Add Login Page (#10)
    * Add Login Page
    
    * Corrige Import

---
+++ pages/Login.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import LoginSection from '../components/LoginSection'
+
+const Login = () => {
+  return (
+    <Layout>
+      <LoginSection />
+    </Layout>
+  )
+}
+
+export default Login
